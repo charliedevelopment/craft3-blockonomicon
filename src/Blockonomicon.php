316cb56582075aa08478b3b141dc696304efae95
Sebastian Bossarte <kommentgames@gmail.com>
2018-01-11 10:16:04 -0600

    Created initial bare plugin.

---
+++ src/Blockonomicon.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Blockonomicon plugin for Craft 3.0
+ * @copyright Copyright Charlie Development
+ */
+
+namespace charliedev\blockonomicon;
+
+use Craft;
+use craft\base\Plugin;
+
+/**
+ * The main Craft plugin class.
+ */
+class Blockonomicon extends Plugin
+{
+	/**
+	 * @inheritdoc
+	 * @see craft\base\Plugin
+	 */
+	public function init()
+	{
+		parent::init();
+	}
+}
