b69f61e8d531f92e9ff71e0846ce45291c188237
Sebastian Bossarte <kommentgames@gmail.com>
2018-02-01 17:58:41 -0600

    Fixed an issue with block description displays/editing.

--- src/controllers/SettingsController.php
+++ src/controllers/SettingsController.php
@@ -64,7 +64,7 @@ class SettingsController extends Controller
 			$block = new StoredBlock();
 			$block->handle = $blocks[$blockhandle]['handle'];
 			$block->name = $blocks[$blockhandle]['name'];
-			$block->description = $blocks[$blockhandle]['description'];
+			$block->description = empty($blocks[$blockhandle]['description']) ? $blocks[$blockhandle]['description'] : '';
 			$block->fields = $blocks[$blockhandle]['fields'];
 		}
 
@@ -104,7 +104,7 @@ class SettingsController extends Controller
 				'name' => $block->name,
 				'handle' => $block->handle,
 				'id' => $block->id,
-				'description' => isset($allblocks[$block->handle]['description']) ? $allblocks[$block->handle]['description'] : '-',
+				'description' => empty($allblocks[$block->handle]['description']) ? $allblocks[$block->handle]['description'] : null,
 				'fields' => count($block->getFieldLayout()->getFieldIds()),
 			];
 		}
@@ -125,7 +125,7 @@ class SettingsController extends Controller
 				'name' => $block['name'],
 				'handle' => $block['handle'],
 				'id' => null,
-				'description' => isset($block['description']) ? $block['description'] : '-',
+				'description' => empty($block['description']) ? $block['description'] : null,
 				'fields' => count($block['fields']),
 			];
 		}
